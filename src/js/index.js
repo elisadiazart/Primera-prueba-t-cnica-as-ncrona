54b9001ed658deb28b5778ab21394da3231057d2
Elisa <elisadiazartcontact@gmail.com>
2023-02-02 20:12:37 +0100

    02.02.23

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,24 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
 
-sayHello();
+
+const dice = document.getElementById('dice')
+const number = document.getElementById('number')
+const advice = document.getElementById('advice')
+
+const newAdvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+  .then(response => response.json())
+  .then( data => {
+    console.log(data.slip.advice)
+      number.textContent = `advice #${data.slip.id}`
+      advice.textContent = `"${data.slip.advice}"`
+    });
+}
+newAdvice()
+
+dice.addEventListener('click', () => {
+    newAdvice()
+})
+
+  
